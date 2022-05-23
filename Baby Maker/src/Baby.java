c5fb75304b35734ded40bf4ee4f0067545cb6ec3
abigail.callahan <abigail.callahan@MU-C1-02.mullen.pvt.k12.co.us>
2022-05-23 12:46:20 -0600

    done

--- Baby Maker/src/Baby.java
+++ Baby Maker/src/Baby.java
@@ -1,9 +1,9 @@
 
 public class Baby
 	{
-		String name; 
-		double weight;
-		boolean isGirl;
+		private String name; 
+		private double weight;
+		private boolean isGirl;
 		
 		public Baby(String n, double w, boolean g)
 		{
@@ -12,6 +12,38 @@ public class Baby
 			isGirl = g;
 		}
 		
+		//not public void because it is returning
+		public String getName()
+			{
+				return name;
+			}
+
+		public void setName(String name)
+			{
+				//or can be name = n;
+				this.name = name;
+			}
+
+		public double getWeight()
+			{
+				return weight;
+			}
+
+		public void setWeight(double weight)
+			{
+				this.weight = weight;
+			}
+
+		public boolean isGirl()
+			{
+				return isGirl;
+			}
+
+		public void setGirl(boolean isGirl)
+			{
+				this.isGirl = isGirl;
+			}
+
 		public static void barf()
 		{
 			System.out.println("Yuck, the baby threw up. They should be cleaned up");
--- Baby Maker/src/BabyMaker.java
+++ Baby Maker/src/BabyMaker.java
@@ -7,17 +7,18 @@ public class BabyMaker
 				Baby myBaby = new Baby("Abigail", 7.4, true);
 				Baby myBaby2 = new Baby("Brian", 5.4, false);
 				
-				System.out.println("My first baby is " + myBaby.name + " and she weighs " + myBaby.weight + " lbs.");
+				System.out.println("My first baby is " + myBaby.getName() + " and she weighs " + myBaby.getWeight() + " lbs.");
+				myBaby.setName("Sadie");
+				System.out.println("My first baby is " + myBaby.getName() + " and she weighs " + myBaby.getWeight() + " lbs.");
 				
-				if(myBaby2.isGirl)
+				if(myBaby2.isGirl())
 					{
 						System.out.println("She is so cute");
 					}
-				else if(!myBaby2.isGirl)
+				else if(!myBaby2.isGirl())
 					{
 						System.out.println("That boy is small");
 					}
-
 			}
 
 	}
